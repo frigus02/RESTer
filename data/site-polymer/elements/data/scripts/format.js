9ad48d0bd6b864a6eb4456f10cbb8ea5c40cf41d
Jan Kuehle <jkuehle90@gmail.com>
2016-10-18 12:00:21 +0200

    Add about, history and settings page.

--- data/site-polymer/elements/data/scripts/format.js
+++ data/site-polymer/elements/data/scripts/format.js
@@ -1,10 +1,15 @@
 (function () {
 
     const self = RESTer.register('format'),
-          formatTime = new Intl.DateTimeFormat('en-US', {hour12: false, hour: '2-digit', minute: '2-digit', second: '2-digit'});
+          formatTime = new Intl.DateTimeFormat(undefined, {hour12: false, hour: '2-digit', minute: '2-digit', second: '2-digit'}),
+          formatDateTime = new Intl.DateTimeFormat(undefined, {year: 'numeric', month: '2-digit', day: '2-digit', hour12: false, hour: '2-digit', minute: '2-digit', second: '2-digit'});
 
     self.time = function (date) {
         return formatTime.format(new Date(date));
+    };
+
+    self.dateTime = function (date) {
+        return formatDateTime.format(new Date(date));
     };
 
 })();
