3e25897545c773b369450b5a2506e9b251c3117a
Jan Kuehle <jkuehle90@gmail.com>
2017-08-09 18:28:25 +0200

    Move custom version of vkBeautify to separate repository

--- src/site/elements/data/workers/format-code.js
+++ src/site/elements/data/workers/format-code.js
@@ -1,6 +1,6 @@
 'use strict';
 
-importScripts('../../../other_components/vkbeautify/vkbeautify.js');
+importScripts('../../../bower_components/vkbeautify/vkbeautify.js');
 
 
 self.onmessage = function (event) {
