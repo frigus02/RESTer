95955cc58091026e819d7fd69d086222c037d0dd
Jan Kuehle <jkuehle90@gmail.com>
2017-11-19 11:43:04 +0100

    Fix helper command in libary-links.md

--- gulpfile.js
+++ gulpfile.js
@@ -267,8 +267,8 @@ function updateLibraryLinks() {
             header: [
                 '# Libary links',
                 'As stated in the post [Improving Review Time by Providing Links to Third Party Sources](https://blog.mozilla.org/addons/2016/04/05/improved-review-time-with-links-to-sources/) it is useful for the addon reviewers to have links to the sources of third party libraries, which are used in the addon.',
-                'Update this file with all changes to used third party libraries (add/remove dependency, change version). Use the following helper:',
-                '    npm run updatelibrarylinks',
+                'Update this file with all changes to used third party libraries (add/remove dependency, change version). Use the helper, which is automatically executed after dependency installation:',
+                '    yarn install',
                 '```\n'
             ].join('\n\n'),
             footer: '\n```\n'
